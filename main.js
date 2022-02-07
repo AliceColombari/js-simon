6b578520013b53081244cd6a8c7c39c69b70501b
AliceColombari <colombari.23@gmail.com>
2022-02-07 13:33:30 +0100

    base generale

---
+++ main.js
@@ -0,0 +1,3 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
